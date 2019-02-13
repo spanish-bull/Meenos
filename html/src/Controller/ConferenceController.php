bb3bd7bf0f823f3bfb70ed455dc6967008603704
Theo Dalleau <theoferah@gmail.com>
2019-02-13 12:43:37 +0100

    create(page): add conference page structure

--- html/src/Controller/ConferenceController.php
+++ html/src/Controller/ConferenceController.php
@@ -2,18 +2,22 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Conference;
 
 class ConferenceController extends AbstractController
 {
     /**
-     * @Route("/conference", name="conference")
+     * @Route("/conference/{id}", name="conference_page")
+     * @param Conference $conference
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index(Conference $conference)
     {
         return $this->render('conference/index.html.twig', [
-            'controller_name' => 'ConferenceController',
+            'conference' => $conference,
         ]);
     }
 }
